ce5b8888bc42cd5fc1f0b479bb66e701f9d6a7c1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-08-25 16:39:22 +0200

    Add shell file to make binary

---
+++ dist.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# build binary distributions for linux/amd64 and darwin/amd64
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "working dir $DIR"
+
+arch=$(go env GOARCH)
+version='0.0.1'
+goversion=$(go version | awk '{print $3}')
+
+for os in linux darwin; do
+    echo "... building v$version for $os/$arch"
+    BUILD=$(mktemp -d -t mapnificent)
+    TARGET="mapnificent-$version.$os-$arch.$goversion"
+    GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build
+    mkdir -p $BUILD/$TARGET
+    cp mapnificent_generator $BUILD/$TARGET/mapnificent
+    pushd $BUILD >/dev/null
+    tar czvf $TARGET.tar.gz $TARGET
+    if [ -e $DIR/dist/$TARGET.tar.gz ]; then
+        echo "... WARNING overwriting dist/$TARGET.tar.gz"
+    fi
+    mv $TARGET.tar.gz $DIR/dist
+    echo "... built dist/$TARGET.tar.gz"
+    popd >/dev/null
+done
